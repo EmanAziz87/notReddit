7f5388711dd502e58e29beed23e4f78c49050711
eman467 <aziz.eman@hotmail.com>
2026-02-01 13:57:50 -0500

    setup express-session configuration with prisma in main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,49 @@
 import express from "express";
+import cors from "cors";
 import postRoutes from "./routes/postRoutes/posts.route.js";
+import session from "express-session";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "../generated/prisma/client.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+if (!process.env.SESSION_SECRET) {
+  throw new Error("SESSION_SECRET must be defined in .env file");
+}
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+    }),
+  }),
+);
 
 app.use("/posts", postRoutes);
 
