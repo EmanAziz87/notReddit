bd917ba45537463035218af949ba65b62f3a3c68
eman467 <aziz.eman@hotmail.com>
2026-03-16 22:05:50 -0400

    created register form on the frontend as well as handler functions and mutations, ready to send data to the backend, just need to refactor register backend route/service to handle profile image

---
+++ frontend/src/pages/Register/Register.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import styles from "./Register.module.css";
+import { useMutation } from "@tanstack/react-query";
+import userService from "../../api/userService";
+
+const Register = () => {
+  const [profileImagePreview, setProfileImagePreview] = useState<string>("");
+
+  const registerUserMutation = useMutation({
+    mutationFn: async (formData: FormData) => userService.register(formData),
+    onError: () => {
+      console.error("error registering user");
+    },
+  });
+
+  const handleRegisterSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("birthdate", e.target["register-birthdate-input"].value);
+    formData.append("email", e.target["register-email-input"].value);
+    formData.append("username", e.target["register-username-input"].value);
+    formData.append("password", e.target["register-password-input"].value);
+    formData.append(
+      "profileImage",
+      e.target["register-profile-pic-input"].value,
+    );
+
+    registerUserMutation.mutate(formData);
+  };
+
+  const handleProfileImagePreview = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const profileImageFile = e.target.files![0];
+    setProfileImagePreview(URL.createObjectURL(profileImageFile));
+  };
+
+  return (
+    <div>
+      <h3>Register Here</h3>
+      <form onSubmit={(e) => handleRegisterSubmit(e)}>
+        <div>
+          <label htmlFor="register-username-input">Username: </label>
+          <input
+            type="text"
+            id="register-username-input"
+            name="register-username-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="register-password-input">Password: </label>
+          <input
+            type="text"
+            id="register-password-input"
+            name="register-password-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="register-profile-pic-input">Profile Picture: </label>
+          <input
+            type="file"
+            onChange={(e) => handleProfileImagePreview(e)}
+            id="register-profile-pic-input"
+            name="register-profile-pic-input"
+          />
+          <img
+            src={profileImagePreview}
+            alt=""
+            className={styles["profile-pic-preview-image"]}
+          />
+        </div>
+        <div>
+          <label htmlFor="register-email-input">Email: </label>
+          <input
+            type="email"
+            id="register-email-input"
+            name="register-email-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="register-birthdate-input">Birthdate: </label>
+          <input
+            type="date"
+            id="register-birthdate-input"
+            name="register-birthdate-input"
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
