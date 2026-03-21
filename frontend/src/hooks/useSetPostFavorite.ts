77034ee0c62f9923e1f4d184eb8be366b3c1128b
eman467 <aziz.eman@hotmail.com>
2026-03-21 16:15:31 -0400

    added favoriting post component and hook to homefeed component. You can now like/dislike/favorite posts in 3 places: homefeed, community, post details component

--- frontend/src/hooks/useSetPostFavorite.ts
+++ frontend/src/hooks/useSetPostFavorite.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import postService from "../api/postService";
 import type { CachedPost } from "../types";
 import { useRef } from "react";
-import type { PostsWithRelationsNoComments } from "backend";
+import type { PostsWithExtraData, PostsWithRelationsNoComments } from "backend";
 
 export const useSetPostFavorite = (
   communityId: string | undefined,
@@ -31,8 +31,11 @@ export const useSetPostFavorite = (
     const communityPostCache = queryClient.getQueryData<
       PostsWithRelationsNoComments[]
     >(["communityPosts", communityId]);
+    let homefeedPostsCache = queryClient.getQueryData<
+      PostsWithRelationsNoComments[]
+    >(["allPosts"]);
 
-    if (!cache && !communityPostCache) return;
+    if (!cache && !communityPostCache && !homefeedPostsCache) return;
 
     if (cache) {
       queryClient.setQueryData(["post", postId], {
@@ -42,11 +45,25 @@ export const useSetPostFavorite = (
         },
       });
     }
+
     if (communityPostCache) {
       queryClient.setQueryData(
         ["communityPosts", communityId],
         communityPostCache!.map((post) => {
           if (postId === String(post.id)) {
+            return { ...post, favorited: favorite };
+          } else {
+            return post;
+          }
+        }),
+      );
+    }
+
+    if (homefeedPostsCache) {
+      queryClient.setQueryData(
+        ["allPosts"],
+        homefeedPostsCache.map((post) => {
+          if (String(post.id) === postId) {
             return { ...post, favorited: favorite };
           } else {
             return post;
@@ -64,36 +81,53 @@ export const useSetPostFavorite = (
       PostsWithRelationsNoComments[]
     >(["communityPosts", communityId]);
 
-    const nextFavoritedState: boolean = cachedPost
-      ? !cachedPost?.fetchedPost.favorited
-      : !(
-          communityCachedPosts?.find((post) => String(post.id) === postId)
-            ?.favorited ?? false
-        );
+    let homefeedPostsCache = queryClient.getQueryData<
+      PostsWithRelationsNoComments[]
+    >(["allPosts"]);
+
+    const currentCache:
+      | PostsWithExtraData
+      | PostsWithRelationsNoComments
+      | undefined = cachedPost
+      ? cachedPost.fetchedPost
+      : communityCachedPosts
+        ? communityCachedPosts?.find((post) => String(post.id) === postId)
+        : (homefeedPostsCache?.find((post) => String(post.id) === postId) ??
+          undefined);
+
+    if (!currentCache)
+      return console.error("Like/Dislike error with currentCache ternary");
+
+    const nextFavoritedState: boolean = !currentCache.favorited;
+
+    console.log("Next favoritedState: ", nextFavoritedState);
 
     applyOptimisticUpdate(nextFavoritedState);
 
     if (debounceTimer.current) clearTimeout(debounceTimer.current);
     debounceTimer.current = setTimeout(() => {
-      let favoritedFinal: boolean | undefined;
+      let favoritedFinal: boolean;
       const finalCached = queryClient.getQueryData<CachedPost>([
         "post",
         postId,
-      ]);
+      ])?.fetchedPost;
+      const finalCommunityPostsCache = queryClient.getQueryData<
+        PostsWithRelationsNoComments[]
+      >(["communityPosts", communityId]);
 
-      if (!finalCached) {
-        const communityPostsCache = queryClient.getQueryData<
-          PostsWithRelationsNoComments[]
-        >(["communityPosts", communityId]);
+      let finalHomefeedPostsCache = queryClient.getQueryData<
+        PostsWithRelationsNoComments[]
+      >(["allPosts"]);
 
-        favoritedFinal = communityPostsCache?.find(
-          (post) => String(post.id) === postId,
-        )?.favorited;
-      }
+      favoritedFinal = finalCached
+        ? finalCached.favorited
+        : finalCommunityPostsCache
+          ? (finalCommunityPostsCache.find((post) => String(post.id) === postId)
+              ?.favorited ?? false)
+          : (finalHomefeedPostsCache?.find((post) => String(post.id) === postId)
+              ?.favorited ?? false);
 
-      setFavoriteMutation.mutate(
-        finalCached?.fetchedPost.favorited ?? favoritedFinal ?? false,
-      );
+      setFavoriteMutation.mutate(favoritedFinal);
     }, 300);
   };
 
